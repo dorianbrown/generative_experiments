48a8c7e079c49dacfe232ab570623e3bea4f4a72
dorian brown <dorianstuartbrown@gmail.com>
2018-11-19 08:48:54 +0100

    Added published folder with some interesting plots

--- angular_lines.py
+++ angular_lines.py
@@ -1,14 +1,42 @@
 #!/usr/bin/env python
 
-import cairo as cr
 import numpy as np
-from random import choice
 from math import pi
 
-from src.objects import DrawGraph
 from src.draw import Canvas, normalize
 from fn import Fn
 from matplotlib import cm
+
+
+def gen_along_sq(angle, lines, start, end, side):
+    x_mid = np.random.uniform(start - 0.5 * np.tan(angle), end + 0.5 * np.tan(angle), lines)
+
+    _x0 = np.zeros(lines)
+    _x1 = np.ones(lines)
+    if side is "left":
+        _y0 = x_mid + np.tan(angle) * 0.5
+        _y1 = x_mid - np.tan(angle) * 0.5
+    elif side is "right":
+        _y0 = x_mid - np.tan(angle) * 0.5
+        _y1 = x_mid + np.tan(angle) * 0.5
+
+    # y0 < 0
+    _x0 = _x0 - (_y0 / np.tan(angle)) * (_y0 < 0)
+    _y0 = _y0 - _y0 * (_y0 < 0)
+
+    # y0 > 1
+    _x0 = _x0 + ((_y0 - 1) / np.tan(angle)) * (_y0 > 1)
+    _y0 = _y0 + (1 - _y0) * (_y0 > 1)
+
+    # y1 < 0
+    _x1 = _x1 + (_y1 / np.tan(angle)) * (_y1 < 0)
+    _y1 = _y1 - _y1 * (_y1 < 0)
+
+    # y1 > 1
+    _x1 = _x1 - ((_y1 - 1) / np.tan(angle)) * (_y1 > 1)
+    _y1 = _y1 + (1 -_y1) * (_y1 > 1)
+
+    return _x0, _y0, _x1, _y1
 
 
 # Document configuration
@@ -16,58 +44,27 @@ cnv_conf = {
     "fname": f"temp/{Fn().name()}",
     "dpi": 300,
     "width": 10,
-    "height": 20,
+    "height": 15,
     "bg_rgb": [(0.05, 0.05, 0.05), (0.15, 0.15, 0.15)]
 }
 
-n_lines = 2000
-noise = 0.5
+n_lines = 1000
 color_noise_var = 0.1
 cm = cm.jet
-alpha = 0.1
-lw = 0.0000025
+alpha = 0.3
+lw = 0.000001
 
 # Draw to canvas
 with Canvas(**cnv_conf) as cnv:
 
-    # Angle1
-    angle = 0.3 * pi
-    y0 = np.random.uniform(0.1, 0.8, n_lines)
-    x0 = y0*np.tan(angle)
+    x0, y0, x1, y1 = gen_along_sq(angle=0.1 * pi, lines=n_lines, start=0.15, end=0.8, side="left")
 
-    col_mid = choice(y0)
-    col_vec = normalize(np.exp(-np.power(y0 - col_mid + np.random.normal(0, color_noise_var, len(y0)), 2)),
-                        nmin=0, nmax=1)
-    colors = cm(col_vec, alpha=alpha)
+    for x0, y0, x1, y1 in zip(x0, y0, x1, y1):
+        edge = [[x0, y0], [x1, y1]]
+        cnv.draw_edge(edge, lw, (0.8, 0.8, 0.8, alpha))
 
-    for x, y, col in zip(x0, y0, colors):
-        edge = [[0, y], [x, 0]]
-        cnv.draw_edge(edge, lw, col)
+    x0, y0, x1, y1 = gen_along_sq(angle=0.1 * pi, lines=n_lines, start=0.2, end=0.7, side="right")
 
-    # Angle2
-    angle = 0.45 * pi
-    y0 = np.random.uniform(0.15, 0.7, n_lines)
-    x0 = y0*np.tan(angle)
-
-    col_mid = choice(y0)
-    col_vec = normalize(np.exp(-np.power(y0 - col_mid + np.random.normal(0, color_noise_var, len(y0)), 2)),
-                        nmin=0, nmax=1)
-    colors = cm(col_vec, alpha=alpha)
-
-    for x, y, col in zip(x0, y0, colors):
-        edge = [[0, y], [x, 0]]
-        cnv.draw_edge(edge, lw, col)
-
-    # Angle3
-    angle = 0.2 * pi
-    y0 = np.random.uniform(0.6, 1.3, n_lines)
-    x0 = y0*np.tan(angle)
-
-    col_mid = choice(y0)
-    col_vec = normalize(np.exp(-np.power(y0 - col_mid + np.random.normal(0, color_noise_var, len(y0)), 2)),
-                        nmin=0, nmax=1)
-    colors = cm(col_vec, alpha=alpha)
-
-    for x, y, col in zip(x0, y0, colors):
-        edge = [[0, y], [x, 0]]
-        cnv.draw_edge(edge, lw, col)
+    for x0, y0, x1, y1 in zip(x0, y0, x1, y1):
+        edge = [[x0, y0], [x1, y1]]
+        cnv.draw_edge(edge, lw, (0.8, 0.8, 0.8, alpha))
