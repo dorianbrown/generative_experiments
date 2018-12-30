d26846f374ad198ea133f8d12b1c2c50d1707a7a
dorian brown <dorianstuartbrown@gmail.com>
2018-12-30 02:56:49 +0100

    Updates

--- angular_lines.py
+++ angular_lines.py
@@ -3,37 +3,36 @@
 import numpy as np
 from math import pi
 
-from src.draw import Canvas, normalize
+from src.draw import Canvas
 from fn import Fn
 from matplotlib import cm
 
 
 def gen_along_sq(angle, lines, start, end, side):
-    x_mid = np.random.uniform(start - 0.5 * np.tan(angle), end + 0.5 * np.tan(angle), lines)
+    min_x = start - 0.5 * np.tan(angle)
+    max_x = end + 0.5 * np.tan(angle)
+    x_mid = np.linspace(min_x, max_x, lines) + np.random.normal(0, 0.5, lines)
 
     _x0 = np.zeros(lines)
     _x1 = np.ones(lines)
     if side is "left":
         _y0 = x_mid + np.tan(angle) * 0.5
         _y1 = x_mid - np.tan(angle) * 0.5
-    elif side is "right":
+    else:
         _y0 = x_mid - np.tan(angle) * 0.5
         _y1 = x_mid + np.tan(angle) * 0.5
 
     # y0 < 0
-    _x0 = _x0 - (_y0 / np.tan(angle)) * (_y0 < 0)
+    # _x0 = _x0 - (_y0 / np.tan(angle)) * (_y0 < 0)
     _y0 = _y0 - _y0 * (_y0 < 0)
-
     # y0 > 1
-    _x0 = _x0 + ((_y0 - 1) / np.tan(angle)) * (_y0 > 1)
+    # _x0 = _x0 + ((_y0 - 1) / np.tan(angle)) * (_y0 > 1)
     _y0 = _y0 + (1 - _y0) * (_y0 > 1)
-
     # y1 < 0
-    _x1 = _x1 + (_y1 / np.tan(angle)) * (_y1 < 0)
+    # _x1 = _x1 + (_y1 / np.tan(angle)) * (_y1 < 0)
     _y1 = _y1 - _y1 * (_y1 < 0)
-
     # y1 > 1
-    _x1 = _x1 - ((_y1 - 1) / np.tan(angle)) * (_y1 > 1)
+    # _x1 = _x1 - ((_y1 - 1) / np.tan(angle)) * (_y1 > 1)
     _y1 = _y1 + (1 -_y1) * (_y1 > 1)
 
     return _x0, _y0, _x1, _y1
@@ -50,21 +49,33 @@ cnv_conf = {
 
 n_lines = 1000
 color_noise_var = 0.1
-cm = cm.jet
-alpha = 0.3
+colormap = cm.gray
+alpha = 0.9
 lw = 0.000001
 
 # Draw to canvas
 with Canvas(**cnv_conf) as cnv:
 
-    x0, y0, x1, y1 = gen_along_sq(angle=0.1 * pi, lines=n_lines, start=0.15, end=0.8, side="left")
+    f = lambda x: np.exp(-x**2)
+    color_values = f(np.linspace(-3, 3, n_lines) + np.random.normal(0, 0.1, n_lines))
+    colors = colormap(color_values)
+    colors[:, 3] = alpha
 
-    for x0, y0, x1, y1 in zip(x0, y0, x1, y1):
+    x0, y0, x1, y1 = gen_along_sq(angle=0.1 * pi, lines=n_lines,
+                                  start=0.15, end=0.8, side="left")
+
+
+    for x0, y0, x1, y1, rgba in zip(x0, y0, x1, y1, colors):
         edge = [[x0, y0], [x1, y1]]
-        cnv.draw_edge(edge, lw, (0.8, 0.8, 0.8, alpha))
+        cnv.draw_edge(edge, lw, rgba)
 
-    x0, y0, x1, y1 = gen_along_sq(angle=0.1 * pi, lines=n_lines, start=0.2, end=0.7, side="right")
+    color_values = f(np.linspace(-3, 3, n_lines) + np.random.normal(0, 0.1, n_lines))
+    colors = colormap(color_values)
+    colors[:, 3] = alpha
 
-    for x0, y0, x1, y1 in zip(x0, y0, x1, y1):
+    x0, y0, x1, y1 = gen_along_sq(angle=0.1 * pi, lines=n_lines,
+                                  start=0.2, end=0.7, side="right")
+
+    for x0, y0, x1, y1, rgba in zip(x0, y0, x1, y1, colors):
         edge = [[x0, y0], [x1, y1]]
-        cnv.draw_edge(edge, lw, (0.8, 0.8, 0.8, alpha))
+        cnv.draw_edge(edge, lw, rgba)
